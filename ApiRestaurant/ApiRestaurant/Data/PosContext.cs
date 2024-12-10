f98c4cd2e2229a71b73de420f5b97af4e8b4fff7
mknkroth <mknn23g@gmail.com>
2024-12-10 23:09:29 +0700

    ok

---
+++ ApiRestaurant/ApiRestaurant/Data/PosContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using api_restaurant.Models;
+
+namespace api_restaurant.Data
+{
+    public class PosContext : DbContext
+    {
+        public PosContext(DbContextOptions<PosContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
