0f05db9838d57e3bc73235e05cb40ae09d8cc374
mknkroth <mknn23g@gmail.com>
2024-12-10 23:09:08 +0700

    ok

---
+++ ApiRestaurant/ApiRestaurant/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using api_restaurant.Data;
+using api_restaurant.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_restaurant.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly PosContext _context;
+
+        public EmployeeController(PosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> Employee()
+        {
+            try
+            {
+                var employee = await _context.Employees.ToListAsync();
+                return Ok(employee);
+            }
+            catch (Exception ex) 
+            { 
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ ApiRestaurant/ApiRestaurant/Controllers/RegisterController.cs
@@ -0,0 +1,35 @@
+﻿//using api_restaurant.Models;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace api_restaurant.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class RegisterController : ControllerBase
+//    {
+//        private readonly UserManager<IdentityUser> _userManager;
+//        private readonly IConfiguration _configuration;
+
+//        public RegisterController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+//        {
+//            _userManager = userManager;
+//            _configuration = configuration;
+//        }
+
+//        [HttpPost("Register")]
+//        public async Task<IActionResult> Register([FromBody] User user)
+//        {
+//            var users = new IdentityUser { UserName = user.UserName};
+//            var result = await _userManager.CreateAsync(users, user.Password);
+
+//            if (result.Succeeded)
+//            {
+//                await _userManager.AddToRoleAsync(user, user.);
+//                return Ok("User registered.");
+//            }
+
+//            return BadRequest(result.Errors);
+//        }
+//    }
+//}
